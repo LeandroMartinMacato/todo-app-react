4db48c8100934c1844e97862ac00423893562f17
Leandro Macato <shadowslayerhero@gmail.com>
2022-08-28 10:54:24 +0800

    feat: add custom todo

--- src/components/todo.js
+++ src/components/todo.js
@@ -21,7 +21,6 @@ export default function Todo() {
 	let subtitle;
 	const [modalIsOpen, setIsOpen] = React.useState(false);
 	const [formData , setFormData] = React.useState({
-		id: "" ,
 		header: "",
 		body: ""
 	})
@@ -32,13 +31,7 @@ export default function Todo() {
 		head: "Finish React To-do-App",
 		body: "Add all features add,delete,edit,finish",
 		dateCreated: "3/14/2015",
-	},
-	// {
-	// 	id: "2",
-	// 	head: "Start Django",
-	// 	body: "Learn Django create social media",
-	// 	dateCreated: "3/14/2015",
-	// },
+	}
 ]);
 
 	/* ----------------------------- STATE FUNCTIONS ---------------------------- */
@@ -47,7 +40,7 @@ export default function Todo() {
 		setTodoArray(prevTodos =>{
 			const newTodos = [...prevTodos]
 			newTodos.push({
-				id: "69",
+				id:`${todoArray.length + 1}`,
 				isDone: false,
 				head: "Walk the dog",
 				body: "I will be walking the dog tommorow",
@@ -57,16 +50,20 @@ export default function Todo() {
 		})
 	}
 
-	function addTodo(){
-		console.log("TEST: Add todo button")
+
+	function addTodo(formData){
+		console.log("Add Custom Todo");
+		console.log(formData);
+		let date = new Date();
+		let current_date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "-" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 		setTodoArray(prevTodos =>{
 			const newTodos = [...prevTodos]
 			newTodos.push({
-				id: "69",
+				id: `${todoArray.length + 1}`,
 				isDone: false,
-				head: "Walk the dog",
-				body: "I will be walking the dog tommorow",
-				dateCreated: "1/12/2014"
+				head: `${formData.header}`,
+				body: `${formData.body}`,
+				dateCreated:`${current_date}` 
 			})
 			return newTodos;
 		})
@@ -115,6 +112,12 @@ export default function Todo() {
 		console.log(formData);
 	}
 
+	function handleSubmit(event){
+		event.preventDefault()
+		addTodo(formData)
+		setFormData({header: "" , body: ""})
+	}
+
 	/* ---------------------------- ELEMENT RENDERING --------------------------- */
 	const todoElements = todoArray.map(todo => {
 		const {id , head , body , dateCreated ,isDone} = todo
@@ -124,8 +127,10 @@ export default function Todo() {
 	/* -------------------------------- COMPONENT ------------------------------- */
 	return (
 		<div>
+			{/* ------------------------------- Todo Items ------------------------------- */}
             {todoElements}
-			<button onClick={testAddTodo} className="text-center text-indigo-400 font-bold rounded my-4 py-2 w-4/12 focus:outline-none bg-gray-900 border-2 border-indigo-400">Add To-Do</button>
+			{/* -------------------------------            ------------------------------- */}
+			<button onClick={testAddTodo} className="text-center text-indigo-400 font-bold rounded my-4 py-2 w-4/12 focus:outline-none bg-gray-900 border-2 border-indigo-400">DEBUG: +1todo</button>
 
 		<button onClick={openModal} className="text-center text-indigo-400 font-bold rounded my-4 py-2 w-4/12 focus:outline-none bg-gray-900 border-2 border-indigo-400">Create to-do</button>
 			<Modal
@@ -139,16 +144,16 @@ export default function Todo() {
 					<h2 className="text-lg font-bold" ref={(_subtitle) => (subtitle = _subtitle)}>Create To-Do item</h2>
 					<button onClick={closeModal}>❌</button>
 				</div>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div>
-						<label for="Header" class="block text-sm font-medium text-gray-700"> Header </label>
-						<div class="mt-1">
+						<label htmlFor="Header" className="block text-sm font-medium text-gray-700"> Header </label>
+						<div className="mt-1">
 							<input type="text" onChange={handleChange} value={formData.header} name="header" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
 							/>
 						</div>
-						<label for="Body" class="block text-sm font-medium text-gray-700"> Body </label>
-						<div class="mt-1">
-							<textarea id="about" onChange={handleChange} value={formData.body} name="body" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"></textarea>
+						<label htmlFor="Body" className="block text-sm font-medium text-gray-700"> Body </label>
+						<div className="mt-1">
+							<textarea id="about" onChange={handleChange} value={formData.body} name="body" rows="3" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"></textarea>
 						</div>
 					</div>
 					<div className="text-right">
